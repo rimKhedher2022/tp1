f1042eac38f1aa0eb8c025488f1c76b3cd6273f6
rim <rimkhedher0@gmail.com>
2022-04-13 21:47:26 +0100

    init

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/', name: 'home')]
+
+    public function home()
+
+
+    {
+        return $this->render('blog/home.html.twig',
+        ['title' => "bienvenue ici les amis",
+        'age' => 31
+        ] );
+    
+   
+    }
+
+
+
+
+    #[Route('/blog/article/12', name: 'blog_show')]
+    public function show()
+
+
+    {
+        return $this->render('blog/show.html.twig');
+    }
+}
