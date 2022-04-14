c30379afdc1752e033a2b2aba363d648373ac9c8
rim <rimkhedher0@gmail.com>
2022-04-15 00:51:59 +0100

    commit2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(ManagerRegistry $doctrine , ArticleRepository $repo): Response
+    #[Route('/blog', name: 'blog')]
+
+    public function index(ArticleRepository $repo): Response
     {
         $articles = $repo->findAll('Titre de l\'article');// trouver les articles
          
@@ -39,15 +47,64 @@ class BlogController extends AbstractController
 
 
 
+    
+    #[Route('/blog/new', name: 'blog_create')]
+    #[Route('/blog/{id}/edit', name: 'blog_edit')]
+    public function form( Article $article =null ,Request $request , EntityManagerInterface $manager)
+    {
+      if (!$article)
+      {$article = new Article(); }
+         
+
+   
+
+         $form = $this->createFormBuilder($article)
+                        ->add('title')
+                        ->add('content')
+                        ->add('image')
+
+                        ->add('save',SubmitType::class,[
+                            'label'=> 'Enregister'
+                        ])
+                        ->getForm();
+                        $form->handleRequest($request);
+                       // dump($article);
+
+
+
+                       if($form->isSubmitted()&& $form->isValid())
+
+                       {
+
+                        if (!$article->getId())
+                        {
+                            $article->setCreatedAt(new \DateTimeImmutable());
+                        }
+
+                        
+                        $manager->persist($article);
+                        $manager->flush();
+                        return $this->redirectToRoute('blog_show',['id' =>$article->getId()]);
+                       }
+
+
+        return $this->render('blog/create.html.twig',
+        ['formArticle' =>$form->createView()]
+    
+    );
+     }
+
 
     #[Route('/blog/{id}', name: 'blog_show')]
     public function show($id,ManagerRegistry $doctrine , ArticleRepository $repo)
 
     {
-      // $article = new Article();
+      
          $article = $repo->find($id); 
         return $this->render('blog/show.html.twig',['article'=>$article]);
         
         
      }
+
+
 }
