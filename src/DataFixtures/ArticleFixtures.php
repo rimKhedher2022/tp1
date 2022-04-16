9c78c309e0e022f753337e831297b988c247da8f
rim <rimkhedher0@gmail.com>
2022-04-16 13:21:55 +0100

    PréparerSymfony

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,22 +6,39 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use Faker\Factory;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
+
+
     {
-        for ($i=1 ; $i<=10; $i++)
-        {
-            $article = new Article();
-            $article->setTitle("Ici un article avec  n .$i")
-                     ->setContent("<p>contenu de l'article n .$i </p>")
-                     ->setImage("http://via.placeholder.com/350x150")
-                     ->setCreatedAt(new \DateTimeImmutable());
 
-                     $manager->persist($article);
+        $faker = \Faker\Factory::create ('FN_tn');
+        // créer 3 cat faké
+        for ($i=1 ; $i<=3; $i++)
 
-        }
-        $manager->flush();
+     {
+            $category = new Category(); 
+            $category->setTitle($faker->sentence())
+                    ->setDescription($faker->paragraph());
+            $manager->persist($category);
+
+
+        
+          //  $content ='<p>';
+          
+
+
+        
     }
+
+
+    $manager->flush();
+}
+
+
 }
